9b897eecd62703f1e6473c37ba75095f40fe4afa
hyagodevbfr <hyago.sbrito@gmail.com>
2024-01-07 14:59:06 -0300

    Learning about Encapsulation

--- POO/Encapsular/Program.cs
+++ POO/Encapsular/Program.cs
@@ -10,6 +10,24 @@ namespace Encapsular
     {
         static void Main(string[] args)
         {
+            Conta c = new Conta();
+            c.Cliente = "Hyago";
+            //c.Saldo = 100;
+
+            //DEPOSITAR
+            double valor = 100;
+            c.Depositar(valor);
+
+            //SACAR
+            double valor2 = 50;
+            c.Sacar(valor2);
+
+            //RESULTADO SALDO ATUAL
+
+            Console.WriteLine($"Cliente: {c.Cliente}");
+            Console.WriteLine($"Saldo: {c.Saldo}");
+
+            Console.ReadKey( );
 
         }
     }
